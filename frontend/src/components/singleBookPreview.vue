0af892b63706c21daf17524ebb81d28fc62d6e8f
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2025-02-17 14:41:01 +0500

    added the previewBook page

--- frontend/src/components/singleBookPreview.vue
+++ frontend/src/components/singleBookPreview.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container fluid>
     <v-row justify="center">
       <v-col cols="12" md="6" lg="6">
         <div class="bookImageContainer">
@@ -9,13 +9,68 @@
       <v-col cols="12" md="6" lg="6">
         <div class="product-details ml-5 mt-15">
           <h1 class="book-title">{{ book.title }}</h1>
+
           <span class="book-reviews">Reviews</span>
           <v-icon class="star ml-8">mdi-star</v-icon>
           <v-icon class="star">mdi-star</v-icon>
           <v-icon class="star">mdi-star</v-icon>
           <v-icon class="star">mdi-star</v-icon>
           <v-icon class="star">mdi-star</v-icon>
+
+          <div class="book-text">
+            Supercharge your child’s learning journey with this amazingly
+            personalized alphabet book, full of beautiful illustrations and
+            hilarious characters.
+          </div>
+
+          <div class="mt-5 d-flex align-items-center">
+            <v-icon class="baby-icon mt-1">mdi-baby-face-outline</v-icon>
+            <h5 class="baby-icon-text ml-5">Recommended for ages 0-3</h5>
+          </div>
+
+          <div class="mt-5 d-flex align-items-center">
+            <v-icon class="baby-icon mt-1">mdi-home-outline</v-icon>
+            <h5 class="baby-icon-text ml-5">
+              Printed and dispatched in 2-4 working days
+            </h5>
+          </div>
+
+          <div class="mt-8 d-flex align-items-center baby-icon-text book-price">
+            <span>$</span>{{ book.price }} USD
+          </div>
+
+          <div class="mt-8">
+            <v-btn class="add-to-card-btn">Add To Cart</v-btn>
+          </div>
         </div>
+      </v-col>
+    </v-row>
+
+    <!-- quote (image) -->
+    <img
+      class="quote-image mb-15"
+      :src="require('../assets/quote.png')"
+      alt=""
+    />
+
+    <!-- anoter image -->
+    <v-row>
+      <v-col cols="12" md="6" lg="6">
+        <img
+          class="heroes-image"
+          :src="require('../assets/heroes.jpeg')"
+          alt=""
+        />
+      </v-col>
+      <v-col cols="12" md="6" lg="6">
+        <span class="heroes-text"
+          >Watch their eyes light up as they turn each page. Our stories aren’t
+          just tales—they’re tools to spark curiosity, kindness, and courage.
+          Whether snuggled up with a printed book or exploring an e-book on-
+          the-go, every story celebrates everyday heroes and lifelong values.
+          Let’s turn reading time into heart- growing time. Ready to start their
+          journey?</span
+        >
       </v-col>
     </v-row>
   </v-container>
@@ -45,7 +100,7 @@ export default defineComponent({
 <style scoped>
 .bookImageContainer {
   width: 100%;
-  height: 700px;
+  height: 800px;
 }
 
 .bookImage {
@@ -74,5 +129,53 @@ export default defineComponent({
   color: orange;
   font-size: 40px;
   margin-bottom: 17px;
+}
+
+.book-text {
+  font-size: 30px;
+  font-weight: 250;
+}
+
+.baby-icon {
+  font-size: 40px;
+}
+
+.baby-icon-text {
+  font-size: 30px;
+  font-weight: 300;
+}
+
+.book-price {
+  font-size: 45px;
+  font-weight: bolder;
+}
+
+.add-to-card-btn {
+  display: flex;
+  justify-content: center;
+  height: 60px;
+  background-color: #283618;
+  color: white;
+  font-size: 40px;
+  padding: 40px 40px;
+  font-weight: 300;
+  border-radius: 0;
+}
+
+.quote-image {
+  width: 100%;
+}
+
+.heroes-image {
+  width: 100%;
+  border-radius: 150px;
+}
+
+.heroes-text {
+  font-size: 30px;
+  display: flex;
+  justify-content: center;
+  font-weight: thin;
+  margin-top: 140px;
 }
 </style>
